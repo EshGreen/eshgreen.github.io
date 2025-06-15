714a1333c4cf1d46faa9f7221d9cbbf90bb66ebd
Emrin <emrin95@gmail.com>
2025-06-15 17:37:35 +0200

    v0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite"
 
 // https://astro.build/config
 export default defineConfig({
   // site: 'https://esh.green',
-  site: 'https://eshgreen.github.io',
+  site: "https://esh.green",
 
   integrations: [mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
   },
-});
+})
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Esh.Green';
-export const SITE_DESCRIPTION = 'Welcome to Esh.Green!';
+export const SITE_TITLE = "Esh.Green"
+export const SITE_DESCRIPTION = "Building Safer, Greener & More Accessible Infrastructure Across Europe"
